b7092167cbc73364daa0ce1cbaac942299a1eba0
Przemek <przemek.moskal@sonarsource.com>
2024-12-19 11:58:06 +0100

    Update myJSfile.js
    Added writePoem() function

--- src/main/javascript/myJSfile.js
+++ src/main/javascript/myJSfile.js
@@ -4,5 +4,10 @@ let toBe = false;
 function myPoem(){
     if(toBe||!toBe){
         question = true;
+        writePoem();
     }
+}
+
+function writePoem(){
+    console.log("To be or not to be that is the question");
 }
